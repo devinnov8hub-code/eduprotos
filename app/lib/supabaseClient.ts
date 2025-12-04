8b15acf8ada781abed658b156b5f99148cda7b81
Chidinma Nwaeze <chidinmabnwaeze@gmail.com>
2025-12-04 10:22:03 +0100

    random commit

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,6 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
+// const supabaseUrl = 'https://dlflaalirvuusqnaqemz.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
-const supabaseUrl = 'https://dlflaalirvuusqnaqemz.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
